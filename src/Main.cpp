88facf4f055001b8ff06131969e4a91b76047c20
deccer <decccer@outlook.com>
2024-07-07 01:16:55 +0200

    Remove compat context and enable debug context

--- src/Main.cpp
+++ src/Main.cpp
@@ -3370,7 +3370,7 @@ auto main(
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 
     const auto primaryMonitor = glfwGetPrimaryMonitor();
     if (primaryMonitor == nullptr) {
