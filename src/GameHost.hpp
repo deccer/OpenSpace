c940114b516d7de6c9da90f959db840a3def330c
deccer <decccer@outlook.com>
2025-01-12 23:26:57 +0100

    Initialize fields

--- src/GameHost.hpp
+++ src/GameHost.hpp
@@ -9,15 +9,14 @@ struct IGame;
 class TGameHost {
 public:
     auto Run() -> void;
-protected:
 private:
 
     auto LoadGameModule() -> bool;
     auto UnloadGameModule() -> void;
     auto CheckIfGameModuleNeedsReloading() -> bool;
 
-    GLFWwindow* _window;
-    IGame* _game;
-    ModuleHandle _gameModule;
-    std::string _gameModuleFilePath;
+    GLFWwindow* _window = nullptr;
+    IGame* _game = nullptr;
+    ModuleHandle _gameModule = nullptr;
+    std::string _gameModuleFilePath = {};
 };
