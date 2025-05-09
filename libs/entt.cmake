8bb4fcd638012d59425db6aaeee8a51d9b52d480
deccer <decccer@outlook.com>
2025-05-09 23:10:40 +0200

    Update entt dependency 3.13.2 -> 3.15.0

--- libs/entt.cmake
+++ libs/entt.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
     entt
     GIT_REPOSITORY https://github.com/skypjack/entt.git
-    GIT_TAG        v3.13.2
+    GIT_TAG        v3.15.0
     GIT_SHALLOW    TRUE
     GIT_PROGRESS   TRUE
 )
