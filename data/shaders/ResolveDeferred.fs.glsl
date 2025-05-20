6028ce72d45bc7cb77328413297e01a605638b07
deccer <decccer@outlook.com>
2025-05-21 00:13:49 +0200

    Fix fragment clip space position

--- data/shaders/ResolveDeferred.fs.glsl
+++ data/shaders/ResolveDeferred.fs.glsl
@@ -50,7 +50,7 @@ vec3 FresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness) {
 
 vec3 ReconstructFragmentWorldPositionFromDepth(float depth, vec2 screenSize, mat4 invViewProj) {
     float z = depth * 2.0 - 1.0; // [0, 1] -> [-1, 1]
-    vec2 position_cs = gl_FragCoord.xy / screenSize; // [0.5, screenSize] -> [0, 1]
+    vec2 position_cs = gl_FragCoord.xy / (screenSize - 1); // [0.5, screenSize] -> [0, 1]
     vec4 position_ndc = vec4(position_cs * 2.0 - 1.0, z, 1.0); // [0, 1] -> [-1, 1]
 
     // undo view + projection
