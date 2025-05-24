8c1454b71fb59c449841603a1bae00945d8b96aa
deccer <decccer@outlook.com>
2025-05-24 17:07:25 +0200

    Rename vertex types

--- data/shaders/VertexTypes.include.glsl
+++ data/shaders/VertexTypes.include.glsl
@@ -8,11 +8,18 @@ struct TVertexPosition
     TPackedVec3 Position;
 };
 
-struct TVertexNormalTangentUvSign
+struct TPackedVertexNormalTangentUvSign
 {
     uint Normal;
     uint Tangent;
     TPackedVec4 UvAndTangentSign;
+};
+
+struct TVertexNormalTangentUvSign
+{
+    vec4 Normal;
+    vec4 Tangent;
+    vec4 UvAndTangentSign;
 };
 
 #endif // VERTEX_TYPES_INCLUDE_GLSL
